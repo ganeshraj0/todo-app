b641c631e5acfcd0b0da13162741d1ed710e5239
ganeshraj0 <ganesh636164@gmail.com>
2024-10-16 04:33:59 -0700

    todo-list

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ToDo from "./componet/ToDo"
+
+const App = () => {
+  return (
+    <div className="bg-stone-900 grid py-4 min-h-screen">
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
